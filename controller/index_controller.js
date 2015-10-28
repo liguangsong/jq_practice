55cf0002814f41f22edddba419149789fdde8b16
liguangsong <863925367@qq.com>
2015-10-28 16:50:09 +0800

    add css

--- controller/index_controller.js
+++ controller/index_controller.js
@@ -1,13 +1,11 @@
 function set_url() {
-
-    var before_help_function = function () {
-        alert('will go to help')
-    };
-
     set_route($("#help_button"), 'help.html', before_help_function);
     set_route($('#back_button'), 'back.html')
 }
 
+function before_help_function() {
+    alert('will go to help')
+}
 
 $(window).ready(function () {
     set_url()
